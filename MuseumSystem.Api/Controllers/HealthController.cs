3044824c17780d5f5a1f79fa77a9b898b635adce
fsa_LongVH <longvhse183071@fpt.edu.vn>
2025-09-17 09:29:14 +0700

    Config mysql connection

---
+++ MuseumSystem.Api/Controllers/HealthController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MuseumSystem.Infrastructure.DatabaseSetting;
+
+namespace MuseumSystem.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public HealthController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("check-db")]
+        public async Task<IActionResult> CheckDb()
+        {
+            if (await _context.Database.CanConnectAsync())
+            {
+                return Ok("✅ Connected to MySQL successfully!");
+            }
+
+            return StatusCode(500, "❌ Failed to connect to MySQL!");
+        }
+    }
+}
--- MuseumSystem.Api/Program.cs
+++ MuseumSystem.Api/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using MuseumSystem.Infrastructure.DatabaseSetting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,21 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var mysqlVersion = builder.Configuration["DatabaseSettings:MySqlVersion"];
+
+// Check null connection string & version
+
+if (string.IsNullOrEmpty(mysqlVersion) || !Version.TryParse(mysqlVersion, out _))
+{
+    throw new InvalidOperationException("MySQL version is not specified or invalid in configuration. Please check appsetting or appsetting dev");
+}
+
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(connectionString, new MySqlServerVersion(new Version(mysqlVersion))));
 
 var app = builder.Build();
 
